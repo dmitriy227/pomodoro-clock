7bdb79644ad6074d7658d51fa3495ea687576451
Dmitriy Zheltkov <dzzy2twenty@gmail.com>
2019-04-23 22:13:12 -0700

    Added DOM Elements

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 // Collect DOM Elements
+var breakVal = document.querySelector('#break-length');
+var sessionVal = document.querySelector('#session-length');
+var minutesTimer = document.querySelector('#minutes');
+var secondsTimer = document.querySelector('#seconds');
+var timerLabelText = document.querySelector('#timer-label');
+
+var startStopBtn = document.querySelector('#start-stop');
+var resetBtn = document.querySelector('#reset');
